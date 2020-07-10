12224c687ca860081909b5e001c2c7626b4cc059
qiaojiyuan <qiaojiyuan@rigorin.com>
2020-07-10 15:18:27 +0800

    zuul init

---
+++ zuul_test/src/test/java/com/springcloud/zuul_test/ZuulTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.zuul_test;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
